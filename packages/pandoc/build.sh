97b617d504cc5fd12a3f2a3a6e1b53552eeec38b
Aditya Alok <alok@termux.dev>
2023-02-22 19:04:29 +0530

    test: iserv

--- packages/pandoc/build.sh
+++ packages/pandoc/build.sh
@@ -8,6 +8,19 @@ TERMUX_PKG_SHA256=f97956087c61b0f3fef618ea35da9f24bcd90aa409559bcda727631de7a696
 TERMUX_PKG_DEPENDS="libffi"
 TERMUX_PKG_BUILD_DEPENDS="ghc-libs"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--ghc-option=-fexternal-interpreter
+--ghc-option=-pgmi=$TERMUX_PKG_CACHEDIR/bin/iserv-proxy
+--ghc-options=-'opti159.223.171.199 -opti41407'"
+
+termux_step_post_get_source(){
+	local tar_file="$TERMUX_PKG_CACHEDIR/iserv-proxy.tar.xz"
+	termux_download "https://github.com/MrAdityaAlok/ghc-cross-tools/releases/download/iserv-proxy-v9.2.5/iserv-proxy-9.2.5.tar.xz" \
+		"$tar_file" \
+		121a20990447e37fbd32ff37e2b809513e6b31824ae435038703e953a2fdadc5
+	mkdir -p "$TERMUX_PKG_CACHEDIR/bin"
+	tar xf "$tar_file" -C "$TERMUX_PKG_CACHEDIR/bin"
+}
 
 termux_step_post_make_install() {
 	# Will be compressed in massage step.
