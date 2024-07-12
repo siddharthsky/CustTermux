b0b42779ae57866c8f956a71435e31b4beca12ed
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2024-07-12 23:47:50 +0530

    Update chk_boot.sh

--- _BootStrap/etc/chk_boot.sh
+++ _BootStrap/etc/chk_boot.sh
@@ -30,4 +30,10 @@ autoboot() {
 # Main execution
 autoboot
 
+echo -e "The package \e[1;32mcom.termux.api\e[0m is now available."
+
+sleep 3
+
 am startservice -n com.termux/.app.TermuxService -a com.termux.service_execute
+
+
