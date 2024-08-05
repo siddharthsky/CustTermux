4a30d31b2d19f8138dc4917e5534ab29de7087f3
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2024-08-06 02:18:14 +0530

    Update Utils.java
    Added sky_update, sky_reinstalll and sky_switch_terminal

--- app/src/main/java/com/termux/Utils.java
+++ app/src/main/java/com/termux/Utils.java
@@ -1,9 +1,15 @@
 package com.termux;
 
+import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +18,19 @@ import android.widget.Toast;
 import android.content.pm.PackageManager;
 
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.FileProvider;
+
+import com.termux.app.TermuxActivity;
+import com.termux.view.TerminalView;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class Utils {
@@ -88,6 +107,80 @@ public class Utils {
         }
     }
 
+    public static void sky_update(Context context) {
+        Intent intentC = new Intent();
+        intentC.setClassName("com.termux", "com.termux.app.RunCommandService");
+        intentC.setAction("com.termux.RUN_COMMAND");
+        intentC.putExtra("com.termux.RUN_COMMAND_PATH", "/data/data/com.termux/files/home/.skyutils.sh");
+        intentC.putExtra("com.termux.RUN_COMMAND_ARGUMENTS", new String[]{"update"});
+        intentC.putExtra("com.termux.RUN_COMMAND_WORKDIR", "/data/data/com.termux/files/home");
+        intentC.putExtra("com.termux.RUN_COMMAND_BACKGROUND", false);
+        intentC.putExtra("com.termux.RUN_COMMAND_SESSION_ACTION", "0");
+        context.startService(intentC);
+    }
+
+    public static void sky_reinstall(Context context) {
+        SkySharedPref preferenceManager = new SkySharedPref(context);
+        preferenceManager.setKey("isServerSetupDone", null);
+        preferenceManager.setKey("server_setup_isLoginCheck", "Yes");
+        preferenceManager.setKey("server_setup_isAutoboot", "No");
+        preferenceManager.setKey("server_setup_isLocal", "No");
+        preferenceManager.setKey("app_name", "null");
+        preferenceManager.setKey("isExit", "noExit");
+        preferenceManager.setKey("server_setup_isEPG", "Yes");
+        preferenceManager.setKey("server_setup_isGenericBanner", "No");
+        preferenceManager.setKey("server_setup_isSSH", "No");
+
+        Intent intentC = new Intent();
+        intentC.setClassName("com.termux", "com.termux.app.RunCommandService");
+        intentC.setAction("com.termux.RUN_COMMAND");
+        intentC.putExtra("com.termux.RUN_COMMAND_PATH", "/data/data/com.termux/files/home/.skyutils.sh");
+        intentC.putExtra("com.termux.RUN_COMMAND_ARGUMENTS", new String[]{"reinstall2"});
+        intentC.putExtra("com.termux.RUN_COMMAND_WORKDIR", "/data/data/com.termux/files/home");
+        intentC.putExtra("com.termux.RUN_COMMAND_BACKGROUND", false);
+        intentC.putExtra("com.termux.RUN_COMMAND_SESSION_ACTION", "0");
+        context.startService(intentC);
+    }
+
+    public static void lake_alert_confirmation(Context context) {
+        // Create an AlertDialog Builder
+        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(context, R.style.CustomAlertDialogTheme);
+
+        // Set the message and the title
+        builder.setMessage("Do you want to proceed?\n[Note: To exit press back button, reopen]")
+            .setTitle("Confirmation");
+
+        // Add the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                sky_terminal(context);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+                dialog.dismiss();
+            }
+        });
+
+        // Create the AlertDialog
+        android.app.AlertDialog dialog = builder.create();
+
+        // Show the AlertDialog
+        dialog.show();
+    }
+
+    private static void sky_terminal(Context context) {
+        TerminalView terminalView = ((Activity) context).findViewById(R.id.terminal_view);
+
+        // Change focusable properties
+        terminalView.setFocusableInTouchMode(true);
+        terminalView.setFocusable(true);
+    }
+
+
     public static void sky_epg_on(Context context) {
         Intent intentC = new Intent();
         intentC.setClassName("com.termux", "com.termux.app.RunCommandService");
@@ -115,7 +208,7 @@ public class Utils {
     public static void showAlertbox_playlist(Context context, DialogInterface.OnClickListener localListener,DialogInterface.OnClickListener publicListener) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.CustomAlertDialogTheme);
         builder.setTitle("Download playlist")
-            .setMessage("Select whether this playlist is for local use only or for other network devices (e.g., TV). \nIf using for other devices, ensure this device has a fixed IP.")
+            .setMessage("Select whether this playlist is for local use only or for other device in network (e.g., TV). \nIf using for other devices, ensure this device has a fixed IP.")
             .setPositiveButton("This device only", localListener)
             .setNegativeButton("For other devices", publicListener);
         AlertDialog dialog = builder.create();
@@ -137,8 +230,35 @@ public class Utils {
         dialog.show();
     }
 
+    public static void sky_ssh_on(Context context) {
+        Intent intentC = new Intent();
+        intentC.setClassName("com.termux", "com.termux.app.RunCommandService");
+        intentC.setAction("com.termux.RUN_COMMAND");
+        intentC.putExtra("com.termux.RUN_COMMAND_PATH", "/data/data/com.termux/files/home/.skyutils.sh");
+        intentC.putExtra("com.termux.RUN_COMMAND_ARGUMENTS", new String[]{"ssh_on"});
+        intentC.putExtra("com.termux.RUN_COMMAND_WORKDIR", "/data/data/com.termux/files/home");
+        intentC.putExtra("com.termux.RUN_COMMAND_BACKGROUND", false);
+        intentC.putExtra("com.termux.RUN_COMMAND_SESSION_ACTION", "0");
+        context.startService(intentC);
+    }
+
+    public static void sky_ssh_off(Context context) {
+        Intent intentC = new Intent();
+        intentC.setClassName("com.termux", "com.termux.app.RunCommandService");
+        intentC.setAction("com.termux.RUN_COMMAND");
+        intentC.putExtra("com.termux.RUN_COMMAND_PATH", "/data/data/com.termux/files/home/.skyutils.sh");
+        intentC.putExtra("com.termux.RUN_COMMAND_ARGUMENTS", new String[]{"ssh_off"});
+        intentC.putExtra("com.termux.RUN_COMMAND_WORKDIR", "/data/data/com.termux/files/home");
+        intentC.putExtra("com.termux.RUN_COMMAND_BACKGROUND", true);
+        intentC.putExtra("com.termux.RUN_COMMAND_SESSION_ACTION", "0");
+        context.startService(intentC);
+    }
 
 
+    public static void updateCustTermux(Context context) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/siddharthsky/CustTermux-JioTVGo/releases"));
+        context.startActivity(browserIntent);
+    }
 }
 
 
