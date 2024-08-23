8eb4b5205d8ab37c67feb7c3db864d864d6b29d5
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2024-08-23 23:14:44 +0530

    Update .termux_updates.sh

--- _BootStrap/etc/.termux_updates.sh
+++ _BootStrap/etc/.termux_updates.sh
@@ -1,1 +1,9 @@
-echo "#####################みらい###############################"
+# Red color for main words and green for the date
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo "##################### みらい ###############################"
+#echo -e "${RED}WARNING:${NC} Please update the app to the latest version whenever it becomes available."
+echo -e "${RED}NOTE:${NC} To be on the safer side, we are renaming the repositories. Stay tuned for the next updates."
+#echo -e "${RED}IMPORTANT:${NC} After ${GREEN}01.09.2024${NC}, you will not be able to install older versions of CustTermux."
