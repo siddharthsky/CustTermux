0fb44dfb4782e0cd543cf99c542b8a7d93db3be2
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2024-08-24 12:37:45 +0530

    Update .termux_updates.sh

--- _BootStrap/etc/.termux_updates.sh
+++ _BootStrap/etc/.termux_updates.sh
@@ -1,9 +1,15 @@
-# Red color for main words and green for the date
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+RED='\033[0;31m'      # Red
+GREEN='\033[0;32m'    # Green
+YELLOW='\033[0;33m'   # Yellow
+NC='\033[0m'          # No Color
 
-echo "##################### みらい ###############################"
+echo "##################### みらい ############################"
 #echo -e "${RED}WARNING:${NC} Please update the app to the latest version whenever it becomes available."
-echo -e "${RED}NOTE:${NC} To be on the safer side, we are renaming the repositories. Stay tuned for the next updates."
+#echo -e "${RED}NOTE:${NC} To be on the safer side, we are renaming the repositories. Stay tuned for the next updates."
 #echo -e "${RED}IMPORTANT:${NC} After ${GREEN}01.09.2024${NC}, you will not be able to install older versions of CustTermux."
+
+echo ""
+
+echo -e "${RED}NOTICE:${NC} ${YELLOW}To avoid potential copyright issues with Jio,${NC}"
+echo -e "${YELLOW}we will be changing the name of our GitHub repository.${NC}"
+echo -e "${GREEN}Updates will be provided soon.${NC}"
