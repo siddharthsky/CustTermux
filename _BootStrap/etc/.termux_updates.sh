0bb6c6ae804f4fa4de4277fe5ae439994d02396c
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2024-09-21 11:53:32 +0530

    Update .termux_updates.sh

--- _BootStrap/etc/.termux_updates.sh
+++ _BootStrap/etc/.termux_updates.sh
@@ -36,14 +36,14 @@ echo "SDK API: $(getprop ro.build.version.sdk), AV: $(getprop ro.build.version.r
 Setup_Prerequisites() {
     # Ensure the .termux directory exists
     mkdir -p "$HOME/.termux"
-    echo "ECHO MEW"
+    # echo "ECHO MEW"
 
     # Check if termux.properties exists and if the desired string is missing
     if ! grep -q "allow-external-apps = true" "$HOME/.termux/termux.properties" 2>/dev/null; then
         # Append the required string, creating the file if it doesn't exist
         echo "allow-external-apps = true" >> "$HOME/.termux/termux.properties"
         chmod 755 "$HOME/.termux/termux.properties"
-         echo "ECHO MEW DONE"
+         # echo "ECHO MEW DONE"
     fi
 }
 
