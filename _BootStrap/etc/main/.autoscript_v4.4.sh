b009c46e24210a00997aa31a8af87246f6712ad1
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2024-09-21 11:47:56 +0530

    Update .autoscript_v4.4.sh

--- _BootStrap/etc/main/.autoscript_v4.4.sh
+++ _BootStrap/etc/main/.autoscript_v4.4.sh
@@ -167,9 +167,10 @@ Setup_Prerequisites() {
     chmod 755 $HOME/.termux/termux.properties
     echo "allow-external-apps = true" >> $HOME/.termux/termux.properties
 
-    # FILE_URL="https://bit.ly/setpasswordexp"
-    # curl -SL --progress-bar --retry 2 --retry-delay 2 -o "$HOME/.set_password.exp" "$FILE_URL" || { echo "Failed to download binary"; exit 1; }
-    # chmod 755 "$HOME/.set_password.exp"
+    FILE_URL="https://raw.githubusercontent.com/siddharthsky/CustTermux-JioTVGo/main/_BootStrap/etc/.set_password.exp"
+    #FILE_URL="https://bit.ly/setpasswordexp" #^redirects here
+    curl -SL --progress-bar --retry 2 --retry-delay 2 -o "$HOME/.set_password.exp" "$FILE_URL" || { echo "Failed to download binary"; exit 1; }
+    chmod 755 "$HOME/.set_password.exp"
     
     if [ "$SDK_VERSION" -le 23 ]; then
         chmod 400 $PREFIX/libexec/termux-am/am.apk
@@ -251,9 +252,9 @@ Setup_Extra() {
 
 SDK_VERSION=$(getprop ro.build.version.sdk)
 if [ "$SDK_VERSION" -le 23 ]; then
-    echo "Script: v6.9.6m [5 series]"
+    echo "Script: v6.9.6n [5 series]"
 else
-    echo "Script: v6.9.6m [7 series]"
+    echo "Script: v6.9.6n [7 series]"
 fi
 
 FILE_PATH="$HOME/.jiotv_go/bin/jiotv_go"
