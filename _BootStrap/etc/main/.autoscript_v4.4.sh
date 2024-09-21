202705346a4156baa39e1122383a54bd7c938bf9
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2024-09-21 11:59:30 +0530

    Update .autoscript_v4.4.sh

--- _BootStrap/etc/main/.autoscript_v4.4.sh
+++ _BootStrap/etc/main/.autoscript_v4.4.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Detect the shell
 SHELL_NAME=$(basename "$SHELL")
 
@@ -255,9 +254,9 @@ Setup_Extra() {
 
 SDK_VERSION=$(getprop ro.build.version.sdk)
 if [ "$SDK_VERSION" -le 23 ]; then
-    echo "Script: v6.9.6o [5 series]"
+    echo "Script: v6.9.6p [5 series]"
 else
-    echo "Script: v6.9.6o [7 series]"
+    echo "Script: v6.9.6p [7 series]"
 fi
 
 FILE_PATH="$HOME/.jiotv_go/bin/jiotv_go"
