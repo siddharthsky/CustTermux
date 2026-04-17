a7ece14c597d46f33ea3f02dc567e3d8f85ccae5
Siddharth Kamble <96260439+siddharthsky@users.noreply.github.com>
2026-04-17 20:51:23 +0530

    feat: open app on bg server start

--- app/src/main/java/com/termux/sky/TxBootReceiver.java
+++ app/src/main/java/com/termux/sky/TxBootReceiver.java
@@ -1,11 +1,17 @@
 package com.termux.sky;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.util.Log;
 import android.widget.Toast;
+
+import com.termux.sky.wizard.AutoAppRedirectDialog;
 
 public class TxBootReceiver extends BroadcastReceiver {
 
@@ -18,12 +24,22 @@ public class TxBootReceiver extends BroadcastReceiver {
 
         String mode = SkySharedPref.getAutoStartMode(context);
 
+        SharedPreferences prefs = context.getSharedPreferences("apps", MODE_PRIVATE);
+        SharedPreferences settings = context.getSharedPreferences("settings", MODE_PRIVATE);
+
+        boolean autoStart = settings.getBoolean("auto_start", false);
+        boolean boot_start_app = settings.getBoolean("boot_start_app", false);
+
+        String pkg = prefs.getString("pkg", null);
+        String cls = prefs.getString("activity", null);
+
+
         Log.d("BootReceiver", "AutoStart mode: " + mode);
 
         switch (mode) {
 
             case "background":
-                startTermuxService(context, true);
+                startTermuxService(context, true, autoStart, boot_start_app, pkg, cls);
                 break;
 
             case "foreground":
@@ -37,7 +53,9 @@ public class TxBootReceiver extends BroadcastReceiver {
         }
     }
 
-    private void startTermuxService(Context context, boolean background) {
+    private void startTermuxService(Context context, boolean background, boolean autoStart, boolean boot_start_app, String pkg, String cls) {
+
+        Toast.makeText(context, "[CTx] Running in background.", Toast.LENGTH_LONG).show();
 
         Intent serviceIntent = new Intent();
         serviceIntent.setClassName("com.termux", "com.termux.app.RunCommandService");
@@ -60,6 +78,25 @@ public class TxBootReceiver extends BroadcastReceiver {
         } else {
             context.startService(serviceIntent);
         }
+
+        if (autoStart) {
+            if (boot_start_app) {
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName(pkg, cls));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    context.startForegroundService(serviceIntent);
+                } else {
+                    context.startService(serviceIntent);
+                }
+            }
+        } else {
+            Log.d("BOOT","SKIP");
+        }
+
+
+
     }
 
     private void startTermuxActivity(Context context) {
